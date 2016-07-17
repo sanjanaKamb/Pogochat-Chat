967492eef83c9847fbe08c40a90f3304243bc69e
Sanjana Kambalapally <sanjaba_kamb@gmail.com>
2016-07-17 12:14:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ece496/pogochat_chat/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.ece496.pogochat_chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
