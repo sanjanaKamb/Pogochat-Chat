26116753a6c7f63e1f0fcf8bdd8e277ae844b068
Rayun Mehrab <rayun.mehrab@mail.utoronto.ca>
2016-07-25 22:36:19 -0400

    Missed a place to replace MainActivity with ListActivity

--- app/src/main/java/com/nullterminator/pogochat/LoginActivity.java
+++ app/src/main/java/com/nullterminator/pogochat/LoginActivity.java
@@ -59,7 +59,7 @@ public class LoginActivity extends AppCompatActivity {
         // Check if user is already logged in or not
         if (session.isLoggedIn()) {
             // User is already logged in. Take him to main activity
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            Intent intent = new Intent(LoginActivity.this, ListActivity.class);
             startActivity(intent);
             finish();
         }
